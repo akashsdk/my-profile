6749eed9997d7477c7c04bc2e45ea6b502c85a30
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2022-09-02 14:56:08 +0600

    ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import Home from './Screen/Home';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+
+
+
+import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      akash
+    <BrowserRouter>
+    <div className='App'>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer/>
     </div>
+    </BrowserRouter>
   );
 }
 
