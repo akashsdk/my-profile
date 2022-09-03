987ef7bf41b9155de79c8ae7cce17e188543cefe
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2022-09-03 14:42:57 +0600

    ok

--- src/Screen/Home.js
+++ src/Screen/Home.js
@@ -1,52 +1,65 @@
 import React from 'react'
 import '../Styles/Home.css'
 import ProfileImage from '../Image/ProfileImage.jpeg'
-// import TextAnimation from "react-text-animations";
+import TextAnimation from "react-text-animations";
 import AnimatedText from 'react-animated-text-content';
+import { Layout } from 'antd';
+
+const { Header, Footer, Sider, Content } = Layout;
 
 export default function Home() {
   return (
-    <div className='homeBody'>
-      <h1 className='Text'></h1>
-      <div >
-        {/* Profile Image */}
-        <div className='homeImage'>
-          <div className='homeImage2'>
-            <img src={ProfileImage} className='homeImg'></img>
-            <div>
-              <AnimatedText
-                type="words" // animate words or chars
-                animation={{
-                  x: '200px',
-                  y: '-20px',
-                  scale: 1.1,
-                  ease: 'ease-in-out',
-                }}
-                animationType="float"
-                interval={0.06}
-                duration={0.8}
-                tag="p"
-                className="animated-paragraph"
-                includeWhiteSpaces
-                threshold={0.1}
-                rootMargin="20%"
-              >
-                Content to animate
-              </AnimatedText>;
-            </div>
-            <h3>HI THERE! I AM TASNIM SHAHRIAR AKASH</h3>
-            <div>
-              {/* <TextAnimation.Slide target="Street"
-                            text={['Street', 'Street', 'Street']}>
-                            Street
-                        </TextAnimation.Slide> */}
-              <h1>Profile</h1>
+
+    <Layout>
+      <Layout>
+        <div className='homeBody'>
+          <h1 className='Text'></h1>
+          <div >
+            {/* Profile Image */}
+            <div className='homeImage'>
+              <div className='homeImage2'>
+                <img src={ProfileImage} className='homeImg'></img>
+                <div>
+                  <AnimatedText
+                    type="chars" // animate words or chars
+                    animation={{
+                      x: '200px',
+                      y: '-20px',
+                      scale: 1.1,
+                      ease: 'ease-in-out',
+                    }}
+                    animationType="bounce"
+                    interval={0.6}
+                    duration={0.8}
+                    tag="p"
+                    className="animated-paragraph"
+                    includeWhiteSpaces
+                    threshold={0.1}
+                    rootMargin="20%"
+                  >
+                    HI THERE! I AM 'AKASH'.
+                  </AnimatedText>
+                </div>
+                <div className='homeBody'>
+                  <h1>
+                    <TextAnimation.Slide target="App Developer" className
+                      text={['[ App Developer ]', '[ Web Developer ]', '[ Freelancer ]', '[ Photographer ]']}>
+                      App Developer
+                    </TextAnimation.Slide>
+                  </h1>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        {/* INTRODUCTION */}
-        <div></div>
-      </div>
-    </div>
+      </Layout>
+      <Layout>
+        
+      </Layout>
+
+
+    </Layout>
+
+
   )
 }
