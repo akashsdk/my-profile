ef3608541e93ab95970f9b35a7770a40336e3173
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2022-09-12 02:02:34 +0600

    okk

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,14 +7,17 @@ import 'react-slideshow-image/dist/styles.css';
 export default function Header() {
   const images = [
     "https://img.freepik.com/free-photo/black-brick-wall-textured-background_53876-63572.jpg?w=1480&t=st=1662908824~exp=1662909424~hmac=1ad17b6a901a16f6bf6ef09042c26cb0810c2f5ab2a929baa08b38444467ea94",
-    "https://images.pexels.com/photos/1013516/pexels-photo-1013516.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    "https://media.istockphoto.com/photos/straight-road-background-with-cloudy-sky-picture-id1206523388?k=20&m=1206523388&s=612x612&w=0&h=60Sh0mq_ilIEHz3HUQgOVHhZB6dISVgYkas6ruCEWI4=",
+    "https://img.freepik.com/free-vector/black-carbon-fiber-texture-pattern-background_1017-33436.jpg?w=1800&t=st=1662909687~exp=1662910287~hmac=9078945ff6e5f64365fdac9ffb23aa3bb0df40bda937d9b9983b96a735da6593",
+    "https://img.freepik.com/free-vector/abstract-black-texture-background-hexagon_206725-413.jpg?w=1800&t=st=1662909739~exp=1662910339~hmac=29aa8c39447f10bba5b1e609c41213066e1dc97cf1729cf47977c40676741e24",
     "https://img.freepik.com/free-photo/colorful-abstract-nebula-space-background_53876-111356.jpg?w=1480&t=st=1662908731~exp=1662909331~hmac=d7387aad7e5ec6ca9c6f648afeb050bd28803793f9d3d54d8cdc0d1b600af841",
   ];
   return (
     <div>
       <div>
-      <Slide>
+        
+      </div>
+      <div>
+        <Slide>
           <div className="each-slide-effect">
             <div style={{ 'backgroundImage': `url(${images[0]})` }}>
               <p className='HomeStyleP'>Smart Street Lighting System </p>
@@ -22,17 +25,17 @@ export default function Header() {
           </div>
           <div className="each-slide-effect">
             <div style={{ 'backgroundImage': `url(${images[1]})` }}>
-            <p className='HomeStyleP'>Smart Street Lighting System </p>
+              <p className='HomeStyleP'>Smart Street Lighting System </p>
             </div>
           </div>
           <div className="each-slide-effect">
             <div style={{ 'backgroundImage': `url(${images[2]})` }}>
-            <p className='HomeStyleP'>Smart Street Lighting System </p>
+              <p className='HomeStyleP'>Smart Street Lighting System </p>
             </div>
           </div>
           <div className="each-slide-effect">
             <div style={{ 'backgroundImage': `url(${images[3]})` }}>
-            <p className='HomeStyleP'>Smart Street Lighting System </p>
+              <p className='HomeStyleP'>Smart Street Lighting System </p>
             </div>
           </div>
         </Slide>
--- src/Screen/Home.js
+++ src/Screen/Home.js
@@ -3,7 +3,8 @@ import '../Styles/Home.css'
 import ProfileImage from '../Image/ProfileImage.jpeg'
 import TextAnimation from "react-text-animations";
 import AnimatedText from 'react-animated-text-content';
-import { Layout } from 'antd';
+import { Col, Row, Layout } from 'antd';
+
 
 const { Header, Footer, Sider, Content } = Layout;
 
@@ -11,48 +12,56 @@ export default function Home() {
   return (
 
     <Layout>
-      <Layout>
-        <div className='homeBody'>
-          <h1 className='Text'></h1>
-          <div >
-            {/* Profile Image */}
-            <div className='homeImage'>
-              <div className='homeImage2'>
-                <img src={ProfileImage} className='homeImg'></img>
-                <div>
-                  <AnimatedText
-                    type="chars" // animate words or chars
-                    animation={{
-                      x: '200px',
-                      y: '-20px',
-                      scale: 1.1,
-                      ease: 'ease-in-out',
-                    }}
-                    animationType="bounce"
-                    interval={0.6}
-                    duration={0.8}
-                    tag="p"
-                    className="animated-paragraph"
-                    includeWhiteSpaces
-                    threshold={0.1}
-                    rootMargin="20%"
-                  >
-                    HI THERE! I AM 'AKASH'.
-                  </AnimatedText>
-                </div>
-                <div className='homeBody'>
-                  <h1>
-                    <TextAnimation.Slide target="App Developer" className
-                      text={['[ App Developer ]', '[ Web Developer ]', '[ Freelancer ]', '[ Photographer ]']}>
-                      App Developer
-                    </TextAnimation.Slide>
-                  </h1>
+      <Row>
+        <Col span={12}>
+
+        </Col>
+        <Col span={12}>
+          <Layout>
+            <div className='homeBody'>
+              <h1 className='Text'></h1>
+              <div >
+                {/* Profile Image */}
+                <div className='homeImage'>
+                  <div className='homeImage2'>
+                    <img src={ProfileImage} className='homeImg'></img>
+                    <div>
+                      <AnimatedText
+                        type="chars" // animate words or chars
+                        animation={{
+                          x: '200px',
+                          y: '-20px',
+                          scale: 1.1,
+                          ease: 'ease-in-out',
+                        }}
+                        animationType="bounce"
+                        interval={0.6}
+                        duration={0.8}
+                        tag="p"
+                        className="animated-paragraph"
+                        includeWhiteSpaces
+                        threshold={0.1}
+                        rootMargin="20%"
+                      >
+                        HI THERE! I AM 'AKASH'.
+                      </AnimatedText>
+                    </div>
+                    <div className='homeBody'>
+                      <h1>
+                        <TextAnimation.Slide target="App Developer" className
+                          text={['[ App Developer ]', '[ Web Developer ]', '[ Freelancer ]', '[ Photographer ]']}>
+                          App Developer
+                        </TextAnimation.Slide>
+                      </h1>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-      </Layout>
+          </Layout>
+        </Col>
+      </Row>
+
       <Layout>
         <div>inro of my self</div>
       </Layout>
