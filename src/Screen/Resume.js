5cd7c24715b1a693d4ffae03af17f625eea2e43b
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2022-09-15 19:39:16 +0600

    ok

--- src/Screen/Resume.js
+++ src/Screen/Resume.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Resume() {
   return (
-    <div>Resume</div>
+    <div>
+        
+    </div>
   )
 }
--- src/Screen/Wrong.js
+++ src/Screen/Wrong.js
@@ -1,34 +1,36 @@
 import React from 'react'
 import { Button, Result } from 'antd';
-import {  Link, } from "react-router-dom";
+import { Link, } from "react-router-dom";
 
 export default function Wrong() {
     return (
         <div style={{
-            marginTop:'5px',
+            marginTop: '5px',
 
         }}>
             <p style={{
-                opacity:'.4'
+                opacity: '.6'
             }}>Could not find this page !</p>
-        <div style={{
-            marginTop:'50px',
-            justifyContent:'center',
-            alignItems:'center',
-            display:'flex',
-            flexDirection: 'column',
+            <div style={{
+                marginTop: '50px',
+                justifyContent: 'center',
+                alignItems: 'center',
+                display: 'flex',
+                flexDirection: 'column',
 
-        }}>
-            
-            <Result
-                status="500"
-                title=""
-                subTitle=""
-                extra={''}
-            />
-            <p>Sorry, something went wrong.</p>
-            <Link  to="/">Go Back Home</Link>
-        </div>
+            }}>
+
+                <Result
+                    status="500"
+                    title=""
+                    subTitle=""
+                    extra={''}
+                />
+                <p>Sorry, something went wrong.</p>
+                <Link state={{
+                    color: '#fff',
+                }} to="/">Go Back Home</Link>
+            </div>
         </div>
     )
 }
