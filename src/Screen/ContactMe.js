e14135e1efe0e121a86a5d319837b9be5ffbe8c2
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2022-10-03 01:57:35 +0600

    ok

--- src/Screen/ContactMe.js
+++ src/Screen/ContactMe.js
@@ -1,12 +1,17 @@
 import React from 'react'
-import '../Styles/ContactMe.css'
+import '../Styles/ContactMe.css';
+import { Col, Row, Select, Button } from 'antd';
 
-
+const { Option } = Select;
+const handleChange = (value) => {
+    console.log(`selected ${value}`);
+};
 
 export default function ContactMe() {
 
     return (
         <div className='ContactBody'>
+            <div style={{ height: '20px' }} />
             <div className='ContactInputDiv'>
                 <input placeholder="Nmae" className='ContactInput'></input>
             </div>
@@ -18,15 +23,28 @@ export default function ContactMe() {
                     <input placeholder="Phone Number" className='ContactInput'></input>
                 </div>
                 <div className='ContactInputDiv2'>
-                    gender
+                    <Select
+                        defaultValue="lucy"
+                        style={{
+                            width: 120,
+                        }}
+                        onChange={handleChange}
+                    >
+                        <Option value="jack">Jack</Option>
+                        <Option value="lucy">Lucy</Option>
+                        <Option value="disabled" disabled>
+                            Disabled
+                        </Option>
+                        <Option value="Yiminghe">yiminghe</Option>
+                    </Select>
                 </div>
             </div>
             <div className='ContactInputDivMess'>
                 <input placeholder="Messege" className='ContactInputMess'></input>
             </div>
             <button type="submit" className="ContactBottuntDiv" >
-            <p className="ContactBottuntH1">Send message</p>
-          </button>
+                <p className="ContactBottuntH1">Send Message</p>
+            </button>
         </div>
     )
 }
