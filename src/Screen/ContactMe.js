c1581209461be2a101a8aea8cd55cdf35b934d38
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2022-10-03 00:22:36 +0600

    ok

--- src/Screen/ContactMe.js
+++ src/Screen/ContactMe.js
@@ -5,11 +5,28 @@ import '../Styles/ContactMe.css'
 
 export default function ContactMe() {
 
-   
-
     return (
-        <div>
-            akash
+        <div className='ContactBody'>
+            <div className='ContactInputDiv'>
+                <input placeholder="Nmae" className='ContactInput'></input>
+            </div>
+            <div className='ContactInputDiv'>
+                <input placeholder="E-mail" className='ContactInput'></input>
+            </div>
+            <div className='ContactInputDiv1'>
+                <div className='ContactInputDiv'>
+                    <input placeholder="Phone Number" className='ContactInput'></input>
+                </div>
+                <div className='ContactInputDiv2'>
+                    gender
+                </div>
+            </div>
+            <div className='ContactInputDivMess'>
+                <input placeholder="Messege" className='ContactInputMess'></input>
+            </div>
+            <button type="submit" className="ContactBottuntDiv" >
+            <p className="ContactBottuntH1">Send message</p>
+          </button>
         </div>
     )
 }
