f4fbd3fd4e28b4064ba773721b74b1ab2a877e12
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2022-10-31 16:26:33 +0600

    ok

--- src/Screen/Resume.js
+++ src/Screen/Resume.js
@@ -3,13 +3,13 @@ import React from 'react'
 export default function Resume() {
   return (
     <div>
-        Resume Resume
+      Resume Resume
 
-        Resume Resume j
-        Resume Resume
+      Resume Resume j
+      Resume Resume
 
 
-Resume Resume
+      Resume Resume
     </div>
   )
 }
